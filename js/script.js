c4723fb4016a2acce9c17163bd9d54d8b3a6c376
Enor Zilkiqi <ez222dc@student.lnu.se>
2023-04-20 15:19:00 +0200

    1.1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var smapiKey = "nxo3vzJa";
+var testDiv;
+
+
+function init() {
+    testDiv = document.getElementById("smapiLoad");
+}
+window.addEventListener("load", init);
+
+function initSmapi() {
+    
+}
+
