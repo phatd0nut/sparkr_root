a92e002c61bb69ad06420c79af9ba382ee46f8ea
Enor Zilkiqi <ez222dc@student.lnu.se>
2023-04-21 11:15:52 +0200

    1.12

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ function init() {
 
 window.addEventListener("load", init);
 
-function initSmapi() {
-    
+function requestSmapi() {
+    let request = new XMLHttpRequest(); 
+    request.responseType =  'json';
+    request.open("GET", "https://smapi.lnu.se/api?api_key=" + smapiKey + "&controller=establishment&method=getall&debug=true", true);
+    request.send(null); 
+    request.onreadystatechange = function () { 
+      if (request.readyState == 4) 
+        if (request.status == 200) getData(request.responseText); 
+        else testDiv.innerHTML = "hej"
+        console.log(requestSmapi);
+    };
 }
