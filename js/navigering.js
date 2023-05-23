b39ad019a614fb5a81690316b271d072d2bf2c89
Enor Zilkiqi <ez222dc@student.lnu.se>
2023-05-23 12:59:56 +0200

    4.0

--- js/navigering.js
+++ js/navigering.js
@@ -1,40 +1,56 @@
 function init() {
     inneMeny();
     uteMeny();
+    
+    let backArrow = document.querySelector(".left-arrow");
+
+    backArrow.addEventListener('touchstart', function () {
+        this.style.transform = 'scale(1.05)';
+    });
+
+    backArrow.addEventListener('touchend', function () {
+        this.style.transform = 'none';
+    });
 }
 
 window.addEventListener("load", init);
 
 function inneMeny() {
     var scalingButtons = document.querySelectorAll(".button3");
-  
-    scalingButtons.forEach(function(button) {
-      button.style.userSelect = 'none';  // Disable user selection
-      button.style.webkitUserSelect = 'none';  // For webkit-based browsers (e.g., Chrome, Safari)
-      button.style.MozUserSelect = 'none';  // For Firefox
-  
-      button.addEventListener('touchstart', function() {
-        this.style.transform = 'scale(1.05)';
-      });
-  
-      button.addEventListener('touchend', function() {
-        this.style.transform = 'none';
-      });
+
+    scalingButtons.forEach(function (button) {
+        // Stänger av att man kan markera för flera olika webbläsare
+        button.style.userSelect = 'none';
+        button.style.webkitUserSelect = 'none';
+        button.style.MozUserSelect = 'none';
+
+        button.addEventListener('touchstart', function () {
+            this.style.transform = 'scale(1.05)';
+        });
+
+        button.addEventListener('touchend', function () {
+            this.style.transform = 'none';
+        });
     });
-  }
-  
+}
+
 
 function uteMeny() {
     var scalingButtons = document.querySelectorAll(".button4");
 
-    scalingButtons.forEach(function(button) {
-      button.addEventListener('touchstart', function() {
-        this.style.transform = 'scale(1.05)';
-      });
-    
-      button.addEventListener('touchend', function() {
-        this.style.transform = 'none';
-      });
+    scalingButtons.forEach(function (button) {
+        // Stänger av att man kan markera för flera olika webbläsare
+        button.style.userSelect = 'none';
+        button.style.webkitUserSelect = 'none';
+        button.style.MozUserSelect = 'none';
+
+        button.addEventListener('touchstart', function () {
+            this.style.transform = 'scale(1.05)';
+        });
+
+        button.addEventListener('touchend', function () {
+            this.style.transform = 'none';
+        });
     });
-    
+
 }
