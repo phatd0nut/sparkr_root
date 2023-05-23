ba75e6446571e5bf4853fa1305dc994a3a1976c4
Enor Zilkiqi <ez222dc@student.lnu.se>
2023-05-23 12:47:12 +0200

    4.0

--- js/navigering.js
+++ js/navigering.js
@@ -1,5 +1,6 @@
 function init() {
     inneMeny();
+    uteMeny();
 }
 
 window.addEventListener("load", init);
@@ -17,4 +18,20 @@ function inneMeny() {
       });
     });
     
+}
+
+function uteMeny() {
+    var scalingButtons = document.querySelectorAll(".button4");
+
+    scalingButtons.forEach(function(button) {
+      button.addEventListener('touchstart', function() {
+        this.style.transform = 'scale(1.05)';
+      });
+    
+      button.addEventListener('touchend', function() {
+        this.style.transform = 'none';
+      });
+    });
+    
+}
 }
